a9251d50a0302930d23d868ea7b5e73d35571b89
ELI Kuo <101239889+Cereal-Kuo@users.noreply.github.com>
2022-03-23 16:51:33 +0800

    Add files via upload

---
+++ Controllers/GetHelpController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoberSpace.Controllers
+{
+    public class GetHelpController : Controller
+    {
+        // GET: GetHelp
+        public ActionResult GetHelp()
+        {
+            return View();
+        }
+
+        public ActionResult Cannabis()
+        {
+            return View();
+        }
+
+        public ActionResult Ecstasy()
+        {
+            return View();
+
+        }
+
+        public ActionResult Heroin()
+        {
+            return View();
+        }
+
+        public ActionResult Meth()
+        {
+            return View();
+        }
+
+        public ActionResult Cocaine()
+        {
+            return View();
+        }
+    }
+}
